1ea83eebe0335d83c270979351c4e709ffc22e97
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-29 00:30:22 +0530

    Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, TypeScript!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//Event Handler
+
+wss.on("connection", function (socket) {
+  console.log("Client connected");
+  setInterval(() => {
+    socket.send("Current price: " + Math.random());
+  }, 500);
+});
