98c686b9089849052d18b80c355af7c4edcebb4d
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-30 01:14:13 +0530

    Message sent and received within a client

--- ChatApp-WS/src/index.ts
+++ ChatApp-WS/src/index.ts
@@ -1,2 +1,16 @@
-// import { WebSocketServer } from "ws";
-// const wss = new WebSocketServer({ port: 8080 });
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+wss.on("connection", (socket) => {
+  // One wss can have many sockets. Each socket for each client on the same wss server
+  userCount += 1;
+  console.log(`User ${userCount} connected`);
+  // Below handles the message coming from the server particularly for this socket only
+  socket.on("message", (msg) => {
+    console.log(`Message received : ${msg.toString()}`);
+    setTimeout(() => {
+      socket.send(msg.toString() + "; from the server");
+    }, 1000);
+  });
+});
