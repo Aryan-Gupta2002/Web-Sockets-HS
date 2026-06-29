3e4772c653d2df84196dbb302572dc2056c9de83
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-30 01:40:37 +0530

    Folder Structuring

--- ChatApp-WS/Chat-backend/src/index.ts
+++ ChatApp-WS/Chat-backend/src/index.ts
@@ -1,16 +1,20 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 wss.on("connection", (socket) => {
+  allSockets.push(socket);
   // One wss can have many sockets. Each socket for each client on the same wss server
   userCount += 1;
   console.log(`User ${userCount} connected`);
   // Below handles the message coming from the server particularly for this socket only
   socket.on("message", (msg) => {
     console.log(`Message received : ${msg.toString()}`);
-    setTimeout(() => {
-      socket.send(msg.toString() + "; from the server");
-    }, 1000);
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      if (!s) return;
+      s.send(msg.toString() + "; from the server");
+    }
   });
 });
---
+++ ChatApp-WS/Chat-frontend/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      function sendMessage() {}
+      <div>
+        <input type="text" placeholder="Enter Your message"></input>
+        <button>Send</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
