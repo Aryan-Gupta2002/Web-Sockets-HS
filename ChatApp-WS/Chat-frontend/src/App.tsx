d3a3e09f77dae66b284a799b00c33b7e087c439a
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-02 00:40:54 +0530

    Basic chat app

--- ChatApp-WS/Chat-frontend/src/App.tsx
+++ ChatApp-WS/Chat-frontend/src/App.tsx
@@ -1,15 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const[messages,setMessage]=useState(["Hi there,welcome to this chat"]);
-  useEffect(()=>{
-    const ws = new WebSocket("ws: //localhost:8080");
-    ws.onmessage =(event) =>{
-      setMessage(m => [...m,event.data]);
-    }
-  },[])
-  }
+  const [messages, setMessage] = useState(["Hi there", "Welcome to this chat"]);
+  let wsRef = useRef<WebSocket | null>(null);
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    wsRef.current = ws;
+    ws.onmessage = (event) => {
+      setMessage((m) => [...m, event.data]);
+    };
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "Room 1",
+          },
+        }),
+      );
+    };
+  }, []);
   return (
     <>
       <div className="h-screen flex flex-col h-screen justify-between">
@@ -21,14 +32,38 @@ function App() {
           ></input>
           <button>Join</button>
         </div> */}
-        <div className="h-[95vh] bg-grey-200"></div>
+        <div className="h-[90vh] bg-gray-400">
+          {messages.map((message) => (
+            <div className="m-8">
+              <span className="bg-white text-black rounded-2xl p-2">
+                {message}
+              </span>
+            </div>
+          ))}
+        </div>
         <div>
           <input
-            className="border-2 p-2 mx-5 my-2 rounded-md"
+            id="message"
+            className="border-2 p-2 mx-5 mb-5 my-2 rounded-md w-[80vw] "
             type="text"
             placeholder="Enter Your message"
           ></input>
-          <button>Send</button>
+          <button
+            className="bg-blue-200 p-2 rounded-xl"
+            onClick={() => {
+              const msg = document.getElementById("message")?.value;
+              wsRef.current.send(
+                JSON.stringify({
+                  type: "chat",
+                  payload: {
+                    message: msg,
+                  },
+                }),
+              );
+            }}
+          >
+            Send
+          </button>
         </div>
       </div>
     </>
