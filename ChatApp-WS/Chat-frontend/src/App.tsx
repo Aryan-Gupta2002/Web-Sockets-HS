4e0098662dfef2c7605f01a728fb1dd724b2ce34
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-01 23:56:18 +0530

    Chat Frontend Development

--- ChatApp-WS/Chat-frontend/src/App.tsx
+++ ChatApp-WS/Chat-frontend/src/App.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const[messages,setMessage]=useState(["Hi there,welcome to this chat"]);
+  useEffect(()=>{
+    const ws = new WebSocket("ws: //localhost:8080");
+    ws.onmessage =(event) =>{
+      setMessage(m => [...m,event.data]);
+    }
+  },[])
+  }
   return (
     <>
-      function sendMessage() {}
-      <div>
-        <input type="text" placeholder="Enter Your message"></input>
-        <button>Send</button>
+      <div className="h-screen flex flex-col h-screen justify-between">
+        {/* <div>
+          <input
+            className="border-2 p-2 mx-5 my-2 rounded-md "
+            type="text"
+            placeholder="Enter room ID"
+          ></input>
+          <button>Join</button>
+        </div> */}
+        <div className="h-[95vh] bg-grey-200"></div>
+        <div>
+          <input
+            className="border-2 p-2 mx-5 my-2 rounded-md"
+            type="text"
+            placeholder="Enter Your message"
+          ></input>
+          <button>Send</button>
+        </div>
       </div>
     </>
   );
